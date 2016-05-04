897bc1521d3915ae2b1e19563b3b5d8045ffffa6
邢尚帅 <1182764909@qq.com>
2016-05-04 11:43:42 +0800

    disCover

--- 合作/DisCoverVC.m
+++ 合作/DisCoverVC.m
@@ -8,15 +8,73 @@
 
 #import "DisCoverVC.h"
 
-@interface DisCoverVC ()
+@interface DisCoverVC ()<UISearchResultsUpdating,UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic, strong) UITableView *tableView;
 
+@property (nonatomic, strong) UISearchController *searchController;
+@property (nonatomic, strong) NSArray *datas;
+@property (nonatomic, strong) NSArray *results;
 @end
 
 @implementation DisCoverVC
+-(void)loadDatas
+{
+    _datas = @[@"Here's", @"to", @"the", @"crazy", @"ones.", @"The", @"misfits.", @"The", @"rebels.", @"The", @"troublemakers.", @"The", @"round", @"pegs", @"in", @"the", @"square", @"holes.", @"The", @"ones", @"who", @"see", @"things", @"differently.", @"They're", @"not", @"fond", @"of", @"rules.", @"And", @"they", @"have", @"no", @"respect", @"for", @"the", @"status", @"quo.", @"You", @"can", @"quote", @"them,", @"disagree", @"with", @"them,", @"glorify", @"or", @"vilify", @"them.", @"About", @"the", @"only", @"thing", @"you", @"can't", @"do", @"is", @"ignore", @"them.", @"Because", @"they", @"change", @"things.", @"They", @"push", @"the", @"human", @"race", @"forward.", @"And", @"while", @"some", @"may", @"see", @"them", @"as", @"the", @"crazy", @"ones,", @"we", @"see", @"genius.", @"Because", @"the", @"people", @"who", @"are", @"crazy", @"enough", @"to", @"think", @"they", @"can", @"change", @"the", @"world,", @"are", @"the", @"ones", @"who", @"do."];
+    _results = _datas;
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self loadDatas];
+    
+    
+    _searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    _searchController.searchResultsUpdater = self;
+    //默认带蒙版
+    _searchController.dimsBackgroundDuringPresentation = NO;
+
+    self.navigationItem.titleView = _searchController.searchBar;
+   
+    _searchController.hidesNavigationBarDuringPresentation = NO;
+
+//    _searchController.searchBar.searchBarStyle = UISearchBarStyleMinimal;
+//    
+//    self.tableView.tableHeaderView = _searchController.searchBar;
+
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    self.tableView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.tableView];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
     // Do any additional setup after loading the view.
+}
+#pragma mark -UISearchResultsUpdating
+//过滤数据
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController{
+    if (searchController.searchBar.text.length == 0) {
+        _results = _datas;
+        
+    }else{
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF CONTAINS[CD] %@",searchController.searchBar.text];
+        _results = [_datas filteredArrayUsingPredicate:predicate];
+    }
+    [self.tableView reloadData];
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return _results.count;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = _results[indexPath.row];
+    
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
